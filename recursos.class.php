3bc8a106a710683e74a1a6ad1833371cc708a95e
FrancoGC <fradagoca@gmail.com>
2016-11-21 18:13:51 -0300

    correción BD

--- recursos.class.php
+++ recursos.class.php
@@ -9,16 +9,16 @@ class recursos{
 	}
 
 	function listarRecursos(){
-		$stmt = $this->db->prepare("SELECT id,titulo,descripcion,archivo FROM documentos ORDER BY fecha");
+		$stmt = $this->db->prepare("SELECT id_documento,titulo,descripcion,ruta_documento FROM documentos ORDER BY fecha_subida");
 		$stmt->execute();
 		$lista = array();
 		
 		while($row = $stmt->fetch()){
 			$fila = array(
-				$row['id'],
+				$row['id_documento'],
 				$row['titulo'],
 				$row['descripcion'],
-				$row['archivo']
+				$row['ruta_documento']
 			);
 			array_push($lista,$fila);
 		}
