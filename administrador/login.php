dd6633f041565392945244eb55a3a7c8e003a526
danielx <danielx-25@hotmail.com>
2016-11-21 17:36:09 -0300

    merge1

--- administrador/login.php
+++ administrador/login.php
@@ -1,9 +1,22 @@
 <?php 
+
+//--------------------------------------------------------------------------
+//aqui se elimina la sesion si se vuelve a entrar al loggin, es solo hasta agregar el boton logout de la Spagina principal
+
+	session_start();
+	if (!isset($_SESSION['nombre'])) {
+	  session_destroy();
+	}
+	
+
+//---------------Redireccionar si ya se inicio la sesion--------------------
+	/*
   session_start();
-  if (isset($_SESSION['rut'])) {
+  if (isset($_SESSION['nombre'])) {
+    //readfile('login.html');
     header('location:index.php');
     exit();
-  }
+  }*/
 
 ?>
 
@@ -21,7 +34,7 @@
                 <img src="img/logoadmin.png" class="enLinea" width="150px">
                 <div id=texto_izquierda class="enLinea aDerecha">
                     <h2 style="font-size: 30px">Museo Mineralogico</h2>
-                    <h3>Plataforma administartiva99999999999999999999999</h3>
+                    <h3>Plataforma administartiva</h3>
                 </div>
 
             </div>
