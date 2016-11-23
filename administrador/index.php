f3c5e03f92c63fd0ed525acdca5cbdff7872188b
Ivan Araya <iaraya1@alumnosuls.cl>
2016-11-22 21:00:09 -0300

    cambio de diseño en el sitio administrador

--- administrador/index.php
+++ administrador/index.php
@@ -22,27 +22,57 @@
     <title></title>
   </head>
   <body onload="init()">
-    <div id="pagina">
-      <div id="header" class="w3-row">
+   <div id="pagina">
+      <!--div id="header" class="w3-row">
         <div class="w3-col" style="width:150px;"><img title="logo" alt="logo" src="img/logoadmin.png" style="margin-left:10px;"></div>
         <div class="w3-rest" >
           <div class="w3-hide-small"><br><img title="logo" alt="logo" src="img/textoadmin.png"><br><br></div>
           <div class="menu-horizontal">
             <ul class="w3-navbar" id="menu-principal">
-              <!--li class="w3-opennav w3-right w3-hide-large">Men&uacute;</li-->
-              <!--li>
-                <a href="cerrar.php">Cerrar Sesi&oacute;n</a>
-              </li-->
             </ul>
           </div>
         </div>
+      </div-->
+		
+		<div id="header" >
+			<div class="w3-row">
+				<div class="w3-col w3-left w3-center"  style="width:130px;height:150px;"><img title="logo ULS" alt="logo ULS" src="img/logoULS.png" style="margin-top:30px;"/></div>
+				<div class="w3-col w3-right w3-center" style="width:150px;height:150px;"><img title="logo Museo" alt="logo Museo" src="img/logoMuseo.png" style="margin-top:9px;"/></div>
+				<div class="w3-rest w3-center w3-hide-small w3-hide-medium" style="height:150px;">
+					<img title="logo" alt="logo" src="img/texto-large.png" style="margin-top:34px;" />
+				</div>
+				<div class="w3-rest w3-center w3-hide-small w3-hide-large" style="height:150px;">
+					<img title="logo" alt="logo" src="img/texto-medium.png" style="margin-top:15px;" />
+				</div>
+			</div>
+			<div class="w3-row menu-horizontal" >
+				<ul class="w3-navbar" id="menu-principal">
+            </ul>
+			</div>			
       </div>
-      <div id="cuerpo" class="w3-row">
+		
+		
+      <div id="cuerpo" class="w3-row" >
 			<div class="w3-panel w3-center">
 				<img src="img/cargando.gif" /><br>
 				<span>Cargando...</span>
 			</div>
 		</div>
-    </div>
+		
+		<div class="w3-row w3-center footer">
+				<div class="w3-third w3-center" style="min-width:300px;">
+					<img src="img/footer1.png" alt="footer sections 01" />
+				</div>
+				<div class="w3-third w3-center w3-hide-medium w3-hide-large" style="min-width:300px;">
+					<a href="http://www.userena.cl/index.php/acreditacion"><img src="img/footer3.png" alt="footer sections 03"  /></a>
+				</div>
+				<div class="w3-third w3-center" style="min-width:300px;">
+					<img src="img/footer2.png" alt="footer sections 02"  />
+				</div>
+				<div class="w3-third w3-center w3-hide-small" style="min-width:300px;">
+					<a href="http://www.userena.cl/index.php/acreditacion"><img src="img/footer3.png" alt="footer sections 03"  /></a>
+				</div>
+			</div>
+   </div>
   </body>
 </html>
